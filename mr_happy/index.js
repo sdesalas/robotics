4887c754e77ed5830cc9f9dd891ada8b3363a3cb
Steven de Salas <steven@desalasworks.com>
2018-04-14 17:58:15 +1000

    Fixes

--- mr_happy/index.js
+++ mr_happy/index.js
@@ -146,10 +146,10 @@ board.on('ready', () => {
   function avoidCollission() {
     console.log('avoidCollission!', averageDistance);
     motors.reverse();
-    board.wait(1000, () => {
+    board.wait(2000, () => {
       if (Math.random() > 0.5) motor_L.stop();
       else motor_R.stop();
-      board.wait(Math.random() * 3000, () => motors.stop());
+      board.wait(Math.random() * 4000, () => motors.stop());
     });
   }
 
